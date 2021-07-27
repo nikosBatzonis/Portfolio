3f69263ccfe6edb8a165afde25d731ad61bd3e74
Nikos Batzonis <n.batzonis@gmail.com>
2021-07-27 15:41:48 +0200

    Adding text on About

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ for (let i = 0; i < portfolio.length; i++) {
                                 </div>
                                 <div class="flip-box-back ">
                                     <h4 class="imgLet">${portfolio[i].them}</h4>
-                                    <a class ="imgLet imgLet-a" href="${portfolio[i].vercel}" style="text-decoration:none;">Look at the project</a>
+                                    <a class ="imgLet imgLet-a" href="${portfolio[i].vercel}" style="text-decoration:none;">Go to the Project</a>
                                  <p class="imgLet">${portfolio[i].skills}</p>
-                                 <a class ="imgLet imgLet-a" href="${portfolio[i].github}" style="text-decoration:none;">Look at the gitHub</a>
+                                 <a class ="imgLet imgLet-a" href="${portfolio[i].github}" style="text-decoration:none;">Go to gitHub</a>
                                 </div>
                             </div>
                         </div>
